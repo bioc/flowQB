2936144cee0158bb48d9df9ae5dd775bc84e62fa
Josef Spidlen <jspidlen@bccrc.ca>
2016-05-26 19:04:05 +0000

    Adding functionality back and refactoring.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowQB@117900 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -21,16 +21,27 @@
 ## software.
 ###############################################################################
 
-find_peak <- function(data, width = .5, fraction = .1)
+## Find density peak in the provided vector of values
+find_peak <- function(data, width = 0.5, fraction = 0.1)
 {
+    ## Additional sanity checks
+    if (width >= 1 || width <= 0) 
+        stop("The width argument shall be between 0 and 1.")
+    if (fraction >= 1 || fraction <= 0)
+        stop("The fraction argument shall be between 0 and 1.")
+    
     x <- sort(data)
     N <- length(data)
     M <- ceiling(N * fraction)
     M2 <- floor((M + 1) / 2)
+    ## Note JS while refactoring: 
+    ## Considering that x is sorted, wouldn't first always be 1 after this?
     for (first in 1:(N - M - 1))
         if (x[first] < x[first + 1])
             break
     
+    ## Note JS while refactoring: 
+    ## Considering that x is sorted, wouldn't last always be N after this?
     for (last in N:(first + M))
         if (x[last - 1] < x[last])
             break
@@ -40,7 +51,9 @@ find_peak <- function(data, width = .5, fraction = .1)
     lo <- first
     hi <- last - M
     
-    # Find the densest fraction (default 10%) of cells
+    ## Find the densest fraction of cells by minimizing the difference between
+    ## the values at the first and "first + fraction size - 1" index of 
+    ## sorted data
     for (j in first:(last - M + 1))
     {
         d <- x[j + M - 1] - x[j]
@@ -52,7 +65,10 @@ find_peak <- function(data, width = .5, fraction = .1)
         }
     }
     
-    # Find the lower bound (default width at half maximum)
+    ## Find the lower bound of the peak based on how much bigger the difference
+    ## between the values at the first and "first + fraction size - 1" index of
+    ## sorted data is allowed to get. By default, the difference can be
+    ## double (i.e., 1 / width)
     for (j in i:first)
     {
         d <- x[j + M - 1] - x[j]
@@ -62,7 +78,11 @@ find_peak <- function(data, width = .5, fraction = .1)
             break
         }
     }
-    # Find the upper bound (default width at half maximum)
+    
+    ## Find the upper bound of the peak based on how much bigger the difference
+    ## between the values at the first and "first + fraction size - 1" index of
+    ## sorted data is allowed to get. By default, the difference can be
+    ## double (i.e., 1 / width)
     for (j in i:(last - M + 1))
     {
         d <- x[j + M - 1] - x[j]
@@ -74,4 +94,34 @@ find_peak <- function(data, width = .5, fraction = .1)
     }
     
     list(lo = x[lo + M2], hi = x[hi + M2])
+}
+
+## TODO: originally called scatter_gate
+## This is applicable to any channels provided as arguments
+## to this function, so it has been renamed, but the rest of the code still
+## needs to be refactored accordingly
+fitted_ellipse_gate <- function(myFlowFrame, channels, R=1)
+{
+    ## Remove events with negative values in the selected channels 
+    ## so that the log transform later doesn't fail
+    for (channel in channels)
+    {
+        values <- exprs(myFlowFrame[,channel])
+        myFlowFrame <- myFlowFrame[values>0]
+    }
+    
+    ## Gate by an ellipse (ellipseoid) on the log transformed
+    ## values of selected channels
+    R2 <- vector(mode="double", nrow(myFlowFrame))
+    for (channel in channels)
+    {
+        values <- log(exprs(myFlowFrame[,channel]))
+        fwhm <- find_peak(values)
+        center <- (fwhm$hi + fwhm$lo)/2
+        radius <- (fwhm$hi - fwhm$lo)/2
+        R2 = R2 + ((values - center)/radius)^2
+    }
+
+    ## Final gate can be stretched/shrunk by specifying R different from 1
+    myFlowFrame[R2 <= R^2]
 }
