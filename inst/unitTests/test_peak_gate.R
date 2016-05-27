67b3911f1e924aff86e7b1412b6e59545f115f0d
Josef Spidlen <jspidlen@bccrc.ca>
2016-05-27 18:00:06 +0000

    New test / fix.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowQB@117931 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_peak_gate.R
+++ inst/unitTests/test_peak_gate.R
@@ -1,9 +1,13 @@
-test_fitted_ellipse_gatek <- function() {
+test_peak_gate <- function() {
     library('flowCore')
     fcsFilePath <- system.file("extdata", "935289.fcs", package="flowQB")
     myFlowFrame <- read.FCS(fcsFilePath)
-    r <- peak_gate(myFlowFrame, 'FSC-H')
 
+    r <- peak_gate(myFlowFrame, 'FSC-H')
     checkTrue(nrow(myFlowFrame[r == TRUE]) == 14133)
     checkTrue(nrow(myFlowFrame[r == FALSE]) == 5867)
+
+    r2 <- peak_gate(myFlowFrame, 'FSC-H', 2)
+    checkTrue(nrow(myFlowFrame[r2 == TRUE]) == 19034)
+    checkTrue(nrow(myFlowFrame[r2 == FALSE]) == 966)
 }
